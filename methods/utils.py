23dc6a7170ed1a6f37fae32d68361ecc38ca91c5
nhammer <nhammer@zoox.com>
2022-12-26 22:58:07 -0800

    Add docstrings to `utils.py`

--- methods/utils.py
+++ methods/utils.py
@@ -1,15 +1,41 @@
-import sys
+"""Define utility functions used for the recommendation systems."""
+from typing import Any, List
+from typing_extensions import Unpack
+
 
 class Accumulator:
-    """For accumulating sums over `n` variables."""
-    def __init__(self, n):
+    """For accumulating sums over some number of variables."""
+
+    # An array of data to accumulate
+    data: List[float]
+
+    def __init__(self, n: int) -> None:
+        """Initialize the data to be accumulated.
+
+        Args:
+            n: The length of the list of data to be accumulated
+        """
         self.data = [0.0] * n
 
-    def add(self, *args):
-        self.data = [a + float(b) for a, b in zip(self.data, args)]
+    def add(self, *variables_to_add: Unpack[Any]) -> None:
+        """Add data to the existing accumulating sums..
 
-    def reset(self):
-        self.data = [0.0] * len(self.data)
+        variables_to_add: variables that can be converted to floats whose
+            argument position corresponds to what element of the accumulator's
+            data array to add to.
+        """
+        for (a, b), i in enumerate(zip(self.data, variables_to_add)):
+            self.data[i] = a + float(b)
 
-    def __getitem__(self, idx):
+    def reset(self) -> None:
+        """Reset the data to accumulate."""
+        for i in range(len(self.data)):
+            self.data[i] = 0.0
+
+    def __getitem__(self, idx: int) -> float:
+        """Get an item from the accumulator.
+
+        Args:
+            idx: The index of the item from the accumulator to get.
+        """
         return self.data[idx]
