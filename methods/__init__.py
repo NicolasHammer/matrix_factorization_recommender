c54edb14e8bcfa35cebaceb29019b05618a40bf2
nhammer <nhammer@zoox.com>
2022-12-26 22:57:20 -0800

    Remove `__init__.py`s

--- methods/__init__.py
+++
@@ -1,7 +0,0 @@
-from .utils import Accumulator
-from .losses import BPR_Loss
-
-from .matrix_factorization import MF
-from .AutoRec import AutoRec
-from .neural_collaborative_filtering import NeuMF as PRDataset
-from .sequence_aware import Caser, SeqDataset
